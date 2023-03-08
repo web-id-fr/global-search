3c397386fbe30ddea8805ac097016503688f4181
yfrommelt <yoann@frommelt.fr>
2023-03-08 12:17:00 +0100

    :zap: use http client pool

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Pool;
+use Illuminate\Http\Client\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
-use Inertia\Response;
-use Octools\Client\OctoolsClient;
 
 class SearchController extends Controller
 {
-    public function __construct(
-        private readonly OctoolsClient $octoolsClient,
-    )
+
+    public function __construct(private readonly Repository $config)
     {
     }
 
-    public function index(Request $request): Response
+    public function index(Request $request): \Inertia\Response
     {
         /** @var string $query */
         $query = $request->input('query');
@@ -25,11 +27,30 @@ class SearchController extends Controller
         ];
 
         if (!empty($query)) {
-            $data['repositories'] = $this->octoolsClient->github->searchRepositories($query);
-            $data['pull_requests'] = $this->octoolsClient->github->searchPullRequests($query);
-            $data['issues'] = $this->octoolsClient->github->searchIssues($query);
+            $responses = Http::pool(fn(Pool $pool) => [
+                $this->auth($pool->as('repositories'))->get("/github/search-repositories/{$query}"),
+                $this->auth($pool->as('pull_requests'))->get("/github/search-pull-requests/{$query}"),
+                $this->auth($pool->as('issues'))->get("/github/search-issues/{$query}"),
+            ]);
+            $jsons = array_map(fn (Response $response) => $response->json(), $responses);
+            $data = array_merge($data, $jsons);
         }
 
         return Inertia::render('Search', $data);
+    }
+
+    private function auth(PendingRequest $request = null): PendingRequest
+    {
+        if ($request === null) {
+            $request = Http::acceptJson();
+        }
+
+        $apiToken = $this->config->get('octools-client.application_token');
+        $apiUrl = $this->config->get('octools-client.octools_api_url');
+
+        return $request
+            ->baseUrl($apiUrl)
+            ->withToken($apiToken)
+            ->throw();
     }
 }
